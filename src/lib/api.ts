d41e0a1e784efb7cb0ff1e525b4cb86603facfdc
Kraken0403 <trivedibhavya1997@gmail.com>
2026-03-26 12:46:02 +0530

    ss

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 const API_BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:8787";
 
-type RequestOptions = RequestInit & {
+type RequestOptions = Omit<RequestInit, "body"> & {
   body?: unknown;
 };
 
